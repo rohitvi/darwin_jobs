7ea80d45f5050508a54077b62264826393f37a15
Rohit <rohit.vish@darwinpgc.com>
2021-07-06 17:59:32 +0530

    Changes

--- app/Views/employer/packages/package_confirmation.php
+++ app/Views/employer/packages/package_confirmation.php
@@ -56,10 +56,66 @@
             data: {
                 package_id: '<?= $id ?>',
             },
-            success: function(data) {
-                const data = $.parseJSON(data);
-                console.log(data);
-                
+            success: function(package) {
+                const NewPackage = $.parseJSON(package);
+                console.log(NewPackage);
+                var options = {
+                    "key": "<?= get_g_setting_val('razorpay_key'); ?>",
+                    "amount": data.price * 100,
+                    "name": <?= get_g_setting_val('application_name'); ?>,
+                    "description": data.title,
+                    "image": <?= get_g_setting_val('logo'); ?>,
+                    "handler": function(response) {
+                        if (response != '') {
+                            var payid = response.razorpay_payment_id;
+                            $.ajax({
+                                url: '<?= base_url('employer/process_payment') ?>',
+                                type: 'POST',
+                                data: {
+                                    package_id: '<?= $id ?>',
+                                    payid: payid
+                                },
+                                beforeSend: function() {
+                                    swal({
+                                        title: 'Dont click back button Transaction in process...',
+                                        allowEscapeKey: false,
+                                        allowOutsideClick: false,
+                                        buttons: false
+                                    });
+                                    $('#paynow').text('Please Wait..');
+
+                                    document.getElementById("shares").readOnly = true;
+                                    document.getElementById("paynow").disabled = true;
+                                },
+                                success: function(data) {
+                                    if (data == 1) {
+                                        swal.close();
+                                        swal({
+                                            title: "Purchased Successfully",
+                                            text: "Payment has been received, Product(s) will be allocated to you within 2 business days.",
+                                            type: "success",
+                                        }).then(function() {
+                                            location.reload();
+                                        });
+                                    } else if (daat == 2) {
+                                        alert("Sorry! something went wrong");
+                                    }
+                                }
+                            });
+                        }
+                    },
+                    "prefill": {
+                        "name": '<?= get_direct_value('employers', 'firstname', 'id', session('employer_id')) . ' ' . get_direct_value('employers', 'lastname', 'id', session('employer_id')) ?>',
+                        "email": '<?= get_direct_value('employers', 'email', 'id', session('employer_id')) ?>',
+                        "contact": '<?= get_direct_value('employers', 'mobile_no', 'id', session('employer_id')) ?>'
+                    },
+                    "theme": {
+                        "color": "#15b8f3" // screen color
+                    }
+                };
+                console.log(options);
+                var propay = new Razorpay(options);
+                propay.open();
             }
         });
     });
