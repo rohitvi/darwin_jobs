d495a6af1c7fd8e4d29c57fb3221d1cd2b31f7bd
Amritpal <amritpal.singh@darwinpgc.com>
2021-07-12 12:27:36 +0530

    ui bug fixes

--- app/Views/users/job_listing.php
+++ app/Views/users/job_listing.php
@@ -141,7 +141,7 @@
               <div class="col-sm-12">
                 <div class="featured_box ">
                   <div class="fb_image">
-                    <img alt="brand logo" src="<?= get_company_logo($job['company_id']) ?>">
+                    <img class="img img-fluid" height="50" width="50" alt="brand logo" src="<?= get_company_logo($job['company_id']) ?>">
                   </div>
                   <div class="fb_content">
                     <h4><?= $job['title']; ?></h4>
