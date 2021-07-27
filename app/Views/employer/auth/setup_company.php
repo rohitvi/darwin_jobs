d2d2d843a892e5b6d0737887cbdade6d4970c06b
Amritpal <amritpal.singh@darwinpgc.com>
2021-07-27 12:11:14 +0530

    added required in employers

--- app/Views/employer/auth/setup_company.php
+++ app/Views/employer/auth/setup_company.php
@@ -48,26 +48,26 @@
                         <div class="col-md-6">
                             <div class="form-group">
                                 <label>Company Name</label>
-                                <input name="company_name" type="text" class="form-control" placeholder="Enter Company Name" value="<?= (isset($data[0]['company_name'])) ? $data[0]['company_name'] : '' ?>">
+                                <input name="company_name" type="text" class="form-control" placeholder="Enter Company Name" value="<?= (isset($data[0]['company_name'])) ? $data[0]['company_name'] : '' ?>" required>
                             </div>
                         </div>
 
                         <div class="col-md-6">
                             <div class="form-group">
                                 <label>Company Email</label>
-                                <input name="email" type="email" class="form-control" placeholder="Enter Company Email" value="<?= $data[0]['email'] ?>">
+                                <input name="email" type="email" class="form-control" placeholder="Enter Company Email" value="<?= $data[0]['email'] ?>" required>
                             </div>
                         </div>
                         <div class="col-md-6">
                             <div class="form-group">
                                 <label>Company Website</label>
-                                <input name="website" type="text" class="form-control" placeholder="Enter Company Website" value="<?= $data[0]['website'] ?>">
+                                <input name="website" type="text" class="form-control" placeholder="Enter Company Website" value="<?= $data[0]['website'] ?>" required>
                             </div>
                         </div>
                         <div class="col-md-6">
                         <div class="form-group">
                             <label>Category</label>
-                            <select name="category" class="form-control js-example-basic-single">
+                            <select name="category" class="form-control js-example-basic-single" required>
                             <option>Select Category</option>
                             <?php foreach($categories as $value) : 
                                 if ($data[0]['category'] == $value['id']) : ?>
@@ -83,13 +83,13 @@
                         <div class="col-md-6">
                             <div class="form-group ">
                                 <label>Founded Date</label>
-                                <input name="founded_date" type="date" class="form-control" placeholder="Enter Founded Date" value="<?= $data[0]['founded_date'] ?>">
+                                <input name="founded_date" type="date" class="form-control" placeholder="Enter Founded Date" value="<?= $data[0]['founded_date'] ?>" required>
                             </div>
                         </div>
                         <div class="col-md-6">
                             <div class="form-group">
                                 <label>Organization Type</label>
-                                <select name="org_type" class="form-control js-example-basic-single">
+                                <select name="org_type" class="form-control js-example-basic-single" required>
                                 <option <?= ($data[0]['org_type'] == 'Public') ? 'selected' : '' ?>>Public</option>
                                 <option <?= ($data[0]['org_type'] == 'Private') ? 'selected' : '' ?>>Private</option>
                                 <option <?= ($data[0]['org_type'] == 'Government') ? 'selected' : '' ?>>Government</option>
@@ -101,7 +101,7 @@
                         <div class="col-md-6">
                             <div class="form-group">
                                 <label>No. of Employers</label>
-                                <select name="no_of_employers" class="form-control select js-example-basic-single">
+                                <select name="no_of_employers" class="form-control select js-example-basic-single" required>
                                 <option value="1-10" <?= ($data[0]['no_of_employers'] == '1-10') ? "selected" : " " ?>>1-10</option>
                                 <option value="10-20" <?= ($data[0]['no_of_employers'] == '10-20') ? "selected" : " " ?>>10-20</option>
                                 <option value="20-30" <?= ($data[0]['no_of_employers'] == '20-30') ? "selected" : " " ?>>20-30</option>
@@ -114,7 +114,7 @@
                         <div class="col-md-6">
                         <div class="form-group">
                             <label>Comapany Description</label>
-                            <input name="description" type="text" class="form-control" placeholder="Enter Company Description" value="<?= $data[0]['description'] ?>">
+                            <input name="description" type="text" class="form-control" placeholder="Enter Company Description" value="<?= $data[0]['description'] ?>" required>
                         </div>
                         </div>
                         <div class="col-md-6 text-right mt-3">
